840d2cc30182a819088f1743087e94b94b3c64f7
Alfie Rayner <alfie.rayner30@gmail.com>
2023-03-20 00:15:37 +0000

    added knowldege to c

--- C & C++/fun.C
+++ C & C++/fun.C
@@ -1,6 +1,30 @@
 #include <stdio.h>
-int main() {
-   // printf() displays the string inside quotation
-   printf("Hello, World!");
-   return 0;
+#include <stdlib.h>
+
+
+int max(int num1, int num2, int num3);
+
+int main()
+{
+    printf("%d", max(40, 10, 50));
+    if(3 < 2 || 2 > 5) {
+        printf("\nTrue");
+    } else {
+        printf("\nFalse");
+    }
+    return 0;
+}
+
+int max(int num1, int num2, int num3) {
+    int result;
+
+    if(num1 >= num2 && num1 >= num3) {
+        result = num1;
+    } else if(num2 >= num1 && num2 >= num3) {
+        result = num2;
+    } else {
+        result = num3;
+    }
+
+    return result;
 }
