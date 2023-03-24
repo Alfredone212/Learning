6fbbc50633b33b8e4061f71d1e2ca3ce31bcd4a1
Alfie Rayner <alfie.rayner30@gmail.com>
2023-03-24 13:14:25 +0000

    changing stuff

--- Webstuff/Js/script.js
+++ Webstuff/Js/script.js
@@ -10,6 +10,7 @@ const milk = {flavour:"Strawberry", type:"Full Fat", price:2.50}
 const cars = ["Volvo", "BMW"]
 cars[0] = "Ferrari"
 cars.push("Ford")
+//alert(cars.length) <-- length of car array
         
 let p = n + 6
 // ^Cannot be redeclared
@@ -20,21 +21,45 @@ y = 5
 //x *= y;
 //alert(x);
 //^Onscreen popup
+
 function hero() {
     return "Hello"
 }
 //alert(milk.flavour + " Is yummy & costs " + milk.price)
+
 let l = mell.length
 //alert(l)
 //alert("We are\"vikings\"")
 // The Backslash is an escape character that allows a "" in ""
 // Slicing for getting number include whitespace
+
 mell.replace("Shmello", "Trello")
 let s = Number.MAX_SAFE_INTEGER
-//alert(s) ^it gives you the highest possible safe interger in js
+//alert(s) ^it gives you the highest possible safe integer in js
+
 n.toString()
 // ^Converts to string
+
+cars.pop()
+// ^Removes the last item in the array
+
+cars.push("Ferrari")
+// ^Adds value to the end of the array
+// .splice is used to get rid of specific objects in an array and is used using the reference
+// numbers such as [pan, pot, sink] pan = 0 in the array and pot = 1 in the array because
+// arrays always start from 0 and .slice creates a new array without the object referenced
+
+cars.sort()
+cars.reverse()
+// Sort sorts it to alphabetical order and reverse reverses it
+
+const dat = new Date()
+// ^Grabs the current date from the browser and you make it show certain dates
         
+
+
 document.getElementById("smeno").innerHTML = p
 document.getElementById("demo").innerHTML = hero()
 document.getElementById("plen").innerHTML = milk.flavour
+document.getElementById("stra").innerHTML = cars.toString()
+document.getElementById("date").innerHTML = dat
