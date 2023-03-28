9773b0ea9e0569ca83641ed72f2288247874d6cd
Alfie Rayner <alfie.rayner30@gmail.com>
2023-03-28 22:07:41 +0100

    changes

--- Webstuff/Js/script.js
+++ Webstuff/Js/script.js
@@ -3,9 +3,11 @@
 If you want a general rule: always declare variables with const.
 If you think the value of the variable can change, use let.
 */
+
+"use strict"
+
 const n = 5
 // ^cannot be reassigned
-
 const mell = "Shmello"
 const milk = {flavour:"Strawberry", type:"Full Fat", price:2.50}
 const cars = ["Volvo", "BMW"]
@@ -17,8 +19,8 @@ let p = n + 6
 // ^Cannot be redeclared
 
 var firstName = "John", lastName = "Paul", age = 26
-x = 10
-y = 5
+// x = 10
+// y = 5
 //x += y;
 //x *= y;
 //alert(x);
@@ -28,6 +30,11 @@ function hero() {
     return "Hello"
 }
 //alert(milk.flavour + " Is yummy & costs " + milk.price)
+//shorter function
+let hello = ""
+hello = () => {
+    return hello
+}
 
 let l = mell.length
 //alert(l)
@@ -60,10 +67,62 @@ const dat = new Date()
 const year = dat.getFullYear()
 const month = dat.getMonth()
 const hours = dat.setMilliseconds(142)
-        
 
+let r = Math.random()
+// Math.max(10, 20) would give 20 because its the largest number
+console.log(r) //^ Gives you a random number with the math function
+
+let o = (age < 18) ? "Too young": "Old enough"
+console.log(o) //^ Compares the values and does something based on that can be used in conjunction
+// with if else statements
+if (age == 18) {
+    let fun =  "You looking fine gyat damn"
+    console.log(fun)
+} else if (age == 26) {
+    let wow = "You a cool boy"
+    console.log(wow)
+} else {
+    let nof =  "To early..."
+    console.log(nof)
+}
+
+/*
+switch(fruits) {
+    case "Banana":
+        console.log("Hello")
+        break;
+    case "Apple":
+        console.log("Welcome")
+        break;   
+    default:
+        console.log("WOW");
+}
+// ^Gives you a different answer based on what you entered
+*/
+
+let i = 0
+for (i=0; i < 10; i++) {
+    console.log(i)
+    if (i == 5) {
+        continue
+    } else if (i == 6) {
+        break
+    }
+}
+// For loops can also iterate through arrays and the if makes it stop the loop at 5 using
+// break
+
+/*
+let h = 0
+while (h < 10) {
+    console.log(i)
+    i++
+}
+loops infinitely if h is less thn 10
+*/
 
 document.getElementById("smeno").innerHTML = p
+document.getElementById("hell").innerHTML = hello()
 document.getElementById("demo").innerHTML = hero()
 document.getElementById("plen").innerHTML = milk.flavour
 document.getElementById("stra").innerHTML = cars.toString()
